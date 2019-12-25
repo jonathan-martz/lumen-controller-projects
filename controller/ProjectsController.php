68ba7eda741d0f92205d1987e0163cede44d8970
jonathan-martz <github@jmartz.de>
2019-12-25 21:23:11 +0100

    remove http request class, add ProjectsProvider and logic for migrations

--- controller/ProjectsController.php
+++ controller/ProjectsController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use \http\Env\Response;
 use Illuminate\Http\JsonResponse;
-use \Illuminate\Http\Request;
-use \Illuminate\Support\Facades\DB;
-use \Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
 /**
@@ -15,147 +13,145 @@ use Illuminate\Validation\ValidationException;
  */
 class ProjectsController extends Controller
 {
+
     /**
-     * @param Request $request
-     * @return Response
-     * @throws ValidationException
+     * @return mixed
      */
-    public function projects(Request $request)
+    public function projects()
     {
-        $validation = $this->validate($request, [
+        $validation = $this->validate($this->request, [
 
         ]);
 
-        $projects = DB::table('projects')->where('UID', '=',$request->user()->getAuthIdentifier())->get();
+        $projects = DB::table('projects')->where('UID', '=', $this->request->user()->getAuthIdentifier())->get();
 
-        $this->addResult('projects',$projects);
+        $this->addResult('projects', $projects);
 
         return $this->getResponse();
     }
 
-    public function get(Request $request)
+    /**
+     * @return mixed
+     */
+    public function get()
     {
-        $validation = $this->validate($request, [
+        $validation = $this->validate($this->request, [
             'id' => 'required|integer'
         ]);
 
-        $id = $request->input('id');
+        $id = $this->request->input('id');
 
         $project = DB::table('projects')
-            ->where('UID', '=',$request->user()->getAuthIdentifier())
-            ->where('id', '=',$id)
+            ->where('UID', '=', $this->request->user()->getAuthIdentifier())
+            ->where('id', '=', $id)
             ->get();
 
-        $this->addResult('project',$project[0]);
+        $this->addResult('project', $project[0]);
 
         return $this->getResponse();
     }
 
     /**
-     * @param Request $request
      * @return JsonResponse
      * @throws ValidationException
      */
-    public function remove(Request $request)
+    public function remove()
     {
-        $validation = $this->validate($request, [
+        $validation = $this->validate($this->request, [
             'id' => 'required|integer'
         ]);
 
-        $id = $request->input('id');
+        $id = $this->request->input('id');
 
         $delete = DB::table('projects')
-            ->where('id', '=',$id)
+            ->where('id', '=', $id)
             ->delete();
 
-        if($delete){
+        if ($delete) {
             $this->addMessage('success', 'Project removed.');
-        }
-        else{
-            $this->addMessage('error', 'Project with id '.$id.' doesnt exists.');
+        } else {
+            $this->addMessage('error', 'Project with id ' . $id . ' doesnt exists.');
         }
 
         return $this->getResponse();
     }
 
-    public function add(Request $request)
+    /**
+     * @return mixed
+     */
+    public function add()
     {
-        $validation = $this->validate($request, [
+        $validation = $this->validate($this->request, [
             'project.name' => 'required|string',
             'project.token' => 'required|string'
         ]);
 
-        $name = $request->input('project.name');
-        $token = $request->input('project.token');
+        $name = $this->request->input('project.name');
+        $token = $this->request->input('project.token');
 
-        $list['name'] = DB::table('projects')->where('name', '=',$name)->where('UID', '=',$request->user()->getAuthIdentifier())->get();
-        $list['token'] = DB::table('projects')->where('token', '=',$token)->where('UID', '=',$request->user()->getAuthIdentifier())->get();
-        $list['both'] = DB::table('projects')->where('token', '=',$token)->where('UID', '=',$request->user()->getAuthIdentifier())->where('name', '=',$name)->get();
+        $list['name'] = DB::table('projects')->where('name', '=', $name)->where('UID', '=', $this->request->user()->getAuthIdentifier())->get();
+        $list['token'] = DB::table('projects')->where('token', '=', $token)->where('UID', '=', $this->request->user()->getAuthIdentifier())->get();
+        $list['both'] = DB::table('projects')->where('token', '=', $token)->where('UID', '=', $this->request->user()->getAuthIdentifier())->where('name', '=', $name)->get();
 
-        if($list['name']->count() === 0 && $list['token']->count() === 0 && $list['both']->count() === 0){
+        if ($list['name']->count() === 0 && $list['token']->count() === 0 && $list['both']->count() === 0) {
 
             $insert = DB::table('projects')->insert([
-                'UID' => $request->user()->getAuthIdentifier(),
+                'UID' => $this->request->user()->getAuthIdentifier(),
                 'name' => $name,
                 'token' => $token
             ]);
 
-            if($insert){
+            if ($insert) {
                 $this->addMessage('success', 'Project added.');
-            }
-            else{
+            } else {
                 $this->addMessage('warning', 'Project something went wrong while adding this project.');
             }
-        }
-        else if($list['name']->count() !== 0){
+        } else if ($list['name']->count() !== 0) {
             $this->addMessage('warning', 'Project with name already exists.');
-        }
-        else if($list['token']->count() !== 0){
+        } else if ($list['token']->count() !== 0) {
             $this->addMessage('warning', 'Project with token already exists.');
-        }
-        else if($list['both']->count() !== 0){
+        } else if ($list['both']->count() !== 0) {
             $this->addMessage('warning', 'Project with name/token already exists.');
-        }
-        else{
+        } else {
             $this->addMessage('error', 'Project something went wrong while adding this project 2.');
         }
 
         return $this->getResponse();
     }
 
-    public function update(Request $request)
+    /**
+     * @return mixed
+     */
+    public function update()
     {
-        $validation = $this->validate($request, [
+        $validation = $this->validate($this->request, [
             'project.name' => 'required|string',
             'project.token' => 'required|string',
             'project.id' => 'required|integer'
         ]);
 
-        $name = $request->input('project.name');
-        $token = $request->input('project.token');
-        $id = $request->input('project.id');
+        $name = $this->request->input('project.name');
+        $token = $this->request->input('project.token');
+        $id = $this->request->input('project.id');
 
-        $list['both'] = DB::table('projects')->where('id', '=',$id)->where('UID', '=',$request->user()->getAuthIdentifier())->get();
+        $list['both'] = DB::table('projects')->where('id', '=', $id)->where('UID', '=', $this->request->user()->getAuthIdentifier())->get();
 
-        if($list['both']->count() === 1){
+        if ($list['both']->count() === 1) {
 
             $update = DB::table('projects')->update([
                 'name' => $name,
                 'token' => $token
             ]);
 
-            if($update){
+            if ($update) {
                 $this->addMessage('success', 'Project updated.');
-            }
-            else{
+            } else {
                 $this->addMessage('warning', 'Project not updated.');
             }
-        }
-        else if($list['both']->count() === 0){
+        } else if ($list['both']->count() === 0) {
             $this->addMessage('warning', 'Project doesnt exists.');
-        }
-        else{
-            $this->addMessage('error', 'Project with id '.$id.' doesnt exists.');
+        } else {
+            $this->addMessage('error', 'Project with id ' . $id . ' doesnt exists.');
         }
 
         return $this->getResponse();
